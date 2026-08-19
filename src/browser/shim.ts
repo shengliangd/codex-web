d09cc7d9924efea6219aff3846d30f0ccd836ad4
dengshengliang <dengshengliang@galbot.com>
2026-08-19 20:28:35 +0800

    Fix browser project folder picker

--- src/browser/shim.ts
+++ src/browser/shim.ts
@@ -322,6 +322,14 @@ function isUnhandledAddWorkspaceRootOptionMessage(value: unknown): value is {
   );
 }
 
+function isPickWorkspaceRootOptionMessage(value: unknown): value is {
+  type: "electron-pick-workspace-root-option";
+} {
+  return (
+    isRecord(value) && value.type === "electron-pick-workspace-root-option"
+  );
+}
+
 function isOpenInBrowserMessage(value: unknown): value is {
   type: "open-in-browser";
   url: string;
@@ -432,6 +440,21 @@ export const ipcRenderer = {
 
       if (isLocalFilePickerMessage(args[0])) {
         return handleLocalFilePickerMessage(args[0]);
+      }
+
+      if (isPickWorkspaceRootOptionMessage(args[0])) {
+        return openSelectWorkspaceRootDialog({
+          listDirectory: requestWorkspaceDirectoryEntries,
+        }).then((root) => {
+          if (root) {
+            window.dispatchEvent(
+              new MessageEvent("message", {
+                data: { type: "workspace-root-option-picked", root },
+              }),
+            );
+          }
+          return undefined;
+        });
       }
 
       if (isUnhandledAddWorkspaceRootOptionMessage(args[0])) {
