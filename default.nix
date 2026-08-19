3ba90859763954a77760775b0415fa08ea50c34d
dengshengliang <dengshengliang@galbot.com>
2026-08-19 18:54:03 +0800

    Pin Codex Desktop version from one source

--- default.nix
+++ default.nix
@@ -16,7 +16,9 @@ flake-utils.lib.eachSystem systems (
   system:
   let
     pkgs = import nixpkgs { inherit system; };
-    appVersion = "26.707.30751";
+    appVersion = builtins.replaceStrings [ "\n" "\r" ] [ "" "" ] (
+      builtins.readFile ./CODEX_DESKTOP_VERSION
+    );
     codexZip = pkgs.fetchurl {
       url = "https://persistent.oaistatic.com/codex-app-prod/ChatGPT-darwin-arm64-${appVersion}.zip";
       hash = "sha256-+BAjhFrlbruYs0nkvIHXtJBTNWSJfOoOpPxKFxBPOJI=";
